52433ada846e102d458adcbb117cf16c0f07a507
dganicolas <dganicolas@rafaelalberti.es>
2025-03-06 03:06:04 +0100

    ahora mejoro un poco lo que viene siendo el tema de listar tareas v2

--- src/main/kotlin/com/es/aplicacion/controller/UsuarioController.kt
+++ src/main/kotlin/com/es/aplicacion/controller/UsuarioController.kt
@@ -31,7 +31,6 @@ class UsuarioController {
 
     @PostMapping("/registrarse")
     fun insert(
-        httpRequest: HttpServletRequest,
         @RequestBody usuarioRegisterDTO: UsuarioRegisterDTO
     ) : ResponseEntity<UsuarioDTO>{
 
