51fdd7e22bfd006c756d9d392d0b21f122d05784
dganicolas <dganicolas@rafaelalberti.es>
2025-03-06 05:36:07 +0100

    vale ahora pilla por id

--- src/main/kotlin/com/es/aplicacion/service/TareaService.kt
+++ src/main/kotlin/com/es/aplicacion/service/TareaService.kt
@@ -37,7 +37,7 @@ class TareaService() {
 
         tareaRepository.save(
             Tarea(
-                idTarea = (tareaRepository.findAll().maxByOrNull { it.idTarea ?: 0 }?.idTarea?.plus(1)) ?: 0,
+                _id = (tareaRepository.findAll().maxByOrNull { it._id ?: 0 }?._id?.plus(1)) ?: 0,
                 nombre = tarea.nombre,
                 descripcion = tarea.descripcion,
                 estado = false,
@@ -48,7 +48,7 @@ class TareaService() {
     }
 
     fun actualizarTarea(authentication: Authentication, id: String): ResponseEntity<String> {
-        val tarea = tareaRepository.findById(id).getOrNull()
+        val tarea = tareaRepository.findBy_id(id).getOrNull()
         if (tarea == null) {
             throw NotFoundException("la tarea no existe")
         } else {
@@ -78,7 +78,7 @@ class TareaService() {
     }
 
     fun eliminarTarea(id: String, authentication: Authentication): ResponseEntity<String> {
-        val tarea = tareaRepository.findById(id).getOrNull() ?: throw NotFoundException("la tarea no existe")
+        val tarea = tareaRepository.findBy_id(id).getOrNull() ?: throw NotFoundException("la tarea no existe")
         if (tarea.autor == authentication.name || authentication.authorities.any { it.authority == "ROLE_ADMIN" }) {
             tareaRepository.delete(tarea)
             return ResponseEntity.ok("la tarea ${tarea.nombre} ha sido eliminada")
